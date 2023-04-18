f76ba5a7ca353f351fab31ff722fbfe03de0b353
Rodney Pilgrim <r.pilgrim@deakin.edu.au>
2023-04-18 14:32:42 +1000

    Tokenization, and data split (test/val)

--- train.py
+++ train.py
@@ -13,3 +13,25 @@ vocab_size = len(vocab_chars)
 
 print("Characters: \"" + "".join(vocab_chars) + "\"")
 print("Number of chars:", vocab_size)
+
+char_to_int = { ch:i for i,ch in enumerate(vocab_chars)}
+int_to_char = { i:ch for i,ch in enumerate(vocab_chars)}
+
+encode = lambda string: [char_to_int[c] for c in string]
+decode = lambda ints: ''.join([int_to_char[i] for i in ints])
+
+plain_text = "Hello There!"
+encoded_text= encode(plain_text)
+print(plain_text, "-> encode:", encoded_text, "-> decode:", decode(encoded_text))
+
+import torch
+data = torch.tensor(encode(text), dtype=torch.long)
+print("data shape & type: ", data.shape, data.dtype)
+print("data[:1000]:")
+print("------------START------------")
+print(data[:1000])
+print("-------------END-------------")
+
+first_90pc_index = 0.9 * len(data)
+train_data = data[:first_90pc_index]
+valid_data = data[first_90pc_index:]
