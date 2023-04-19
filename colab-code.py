3b3f03c284a8c4447bab10a71d79c1be7a827d58
Rodney Pilgrim <r.pilgrim@deakin.edu.au>
2023-04-19 17:39:48 +1000

    The trick in self-attention: matrix multiplication

--- colab-code.py
+++ colab-code.py
@@ -165,3 +165,12 @@ for b in range(B):
 
 print("x[0]: ", x[0])
 print("xbow[0]: ", xbow[0])
+
+torch.manual_seed(42)
+a = torch.tril(torch.ones(3,3))
+a = a / torch.sum(a, 1, keepdim=True)
+b = torch.randint(0,10,(3,2)).float()
+c = a @ b
+print("a=", a)
+print("b=", b)
+print("c=", c)
