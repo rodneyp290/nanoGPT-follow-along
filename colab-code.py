a595c09af5eaaf3ee8f0100e082c64f9f938dc05
Rodney Pilgrim <r.pilgrim@deakin.edu.au>
2023-04-26 16:23:25 +1000

    THE CRUX OF THE VIDEO: v4: self-attention

--- colab-code.py
+++ colab-code.py
@@ -193,3 +193,29 @@ wei = wei.masked_fill(tril == 0, float("-inf"))
 wei = F.softmax(wei, dim=1)
 xbow3 = wei @ x
 print("xbow == xbow3:", torch.allclose(xbow, xbow3))
+
+#version 4: self-attention!
+torch.manual_seed(1337)
+B,T,C = 4, 8, 32
+x = torch.randn(B,T,C)
+
+# let's see a single Head perform self-attention
+head_size = 16
+key = nn.Linear(C, head_size, bias=False)
+query = nn.Linear(C, head_size, bias=False)
+value = nn.Linear(C, head_size, bias=False)
+
+k = key(x) # (B, T, 16)
+q = query(x) # (B, T, 16)
+v = value(x) # (B, T, 16)
+
+wei = q @ k.transpose(-2, -1) # (B, T, 16) @ (B, T, 16) ---> (B, T, T)
+
+tril = torch.tril(torch.ones(T,T))
+#wei = torch.zeros((T,T))
+wei = wei.masked_fill(tril == 0, float("-inf"))
+wei = F.softmax(wei, dim=-1)
+out = wei @ v
+
+print("v4 self-attention out shape", out.shape)
+print("v4 self-attention wei[0]\n", wei[0])
