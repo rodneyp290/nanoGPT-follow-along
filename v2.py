9083977db6659a8544062abf2f5e011cb1988278
Rodney Pilgrim <r.pilgrim@deakin.edu.au>
2023-04-26 18:45:08 +1000

    Add feedforward layers of transformer block

--- v2.py
+++ v2.py
@@ -99,6 +99,19 @@ class MultiHeadAttention(nn.Module):
     def forward(self, x):
         return torch.cat([h(x) for h in self.heads], dim=-1)
 
+class FeedForward(nn.Module):
+    """ a simple linear layer followed by a non-linearity """
+
+    def __init__(self, n_embed):
+        super().__init__()
+        self.net = nn.Sequential(
+            nn.Linear(n_embed,n_embed),
+            nn.ReLU(),
+        )
+
+    def forward(self, x):
+        return self.net(x)
+
 # super simple bigram model
 class BigramLanguageModel(nn.Module):
     def __init__(self):
@@ -107,6 +120,7 @@ class BigramLanguageModel(nn.Module):
         self.token_embedding_table = nn.Embedding(vocab_size, n_embed)
         self.position_embedding_table = nn.Embedding(block_size, n_embed)
         self.sa_heads = MultiHeadAttention(4, n_embed//4) # i.e. 4 heads of 8-dimensional self-attention
+        self.ffwd = FeedForward(n_embed)
         self.lm_head = nn.Linear(n_embed, vocab_size)
 
     def forward(self, idx, targets=None):
@@ -119,6 +133,7 @@ class BigramLanguageModel(nn.Module):
         )  # (T, C)
         x = token_embed + pos_embed
         x = self.sa_heads(x)
+        x = self.ffwd(x)
         logits = self.lm_head(x)  # (B,T,vocab_size)
 
         if targets is None:
